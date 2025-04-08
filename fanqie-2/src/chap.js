dad5be0a1240bbe37dd30ee8b52a16bf6fb05950
jack phong <ngotienphong5141@gmail.com>
2025-04-08 21:48:28 +0700

    [ADD] add new fanqie ext

---
+++ fanqie-2/src/chap.js
@@ -0,0 +1,45 @@
+load("config.js");
+
+function getUrlChapter(chapterId, code) {
+  return `https://api.softrr.cn/api/fanqie/?id=${chapterId}&code=${code}`;
+}
+
+function execute(url) {
+  const regex = /(?:item_id=|\/)(\d+)$/;
+  let chapid = url.match(regex)[1];
+  url = getUrlChapter(chapid, getVerifyCode());
+
+  const response = fetch(url);
+  if (response.ok) {
+    const json = response.json();
+    const htmlContent = json.data;
+    return Response.success(extractChapterContent(htmlContent));
+  } else {
+    return Response.error("API bên thứ 3 đã cook :))");
+  }
+}
+
+function extractChapterContent(htmlString) {
+  const articleStart = htmlString.indexOf("<article>");
+  const articleEnd = htmlString.indexOf("</article>");
+  const articleContent = htmlString.substring(articleStart + 9, articleEnd);
+
+  let result = articleContent
+    .replace(/<p[^>]*>/g, "")
+    .replace(/<\/p>/g, "<br><br>")
+    .trim();
+
+  return result;
+}
+
+function getVerifyCode() {
+  const VERIFY_URL = "https://api.softrr.cn/api/verification/?id=1";
+  const response = fetch(VERIFY_URL);
+  if (response.ok) {
+    const json = response.json();
+    const code = json.code;
+    return code;
+  } else {
+    return Response.error("Lỗi verify code");
+  }
+}
---
+++ fanqie-2/src/config.js
@@ -0,0 +1,7 @@
+let config_host = "https://fanqienovel.com";
+let config_host2 = "http://192.168.1.126:9999";
+// IP của android chạy webservice fanqie
+let host = "http://192.168.1.126:9999";
+if (typeof host !== "undefined") {
+  config_host2 = host;
+}
